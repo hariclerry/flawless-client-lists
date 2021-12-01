e925b513ee08590a63c580e221ff4d6df88dfd3a
Harriet Ayugi <clerryha@gmail.com>
2021-12-02 00:40:47 +0300

    move mock data folder out of pages folder

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { users } from "../mock-data/mockData";
+import { users } from "../../mock-data/mockData";
 
 export default function handler(req, res) {
   res.status(200).json(users)
